2ee88bfb354432fd874957dbafedf1b7af97adda
Lucas Pires Silva <118729077+LucasVPSilva@users.noreply.github.com>
2023-07-24 08:53:17 -0300

    atividade pratica 2 de intro back-end, feito

---
+++ exercicios.js
@@ -0,0 +1,83 @@
+
+// Para acessar cada atividade, basta retirar os comentários.
+
+// Exercicio 1 - Calcular de Celsius para Fahrenheit
+
+/*
+const tempCelsius = Number(prompt('Informe a temperatura em Celsius: '));
+
+alert(`A temperatura ${tempCelsius} Celsius equivale a ${(tempCelsius * 1.8) + 32} Fahrenheit`)
+*/
+
+
+// Exercicio 2 - Crie um programa que peça um número de 1 a 7 e exiba o dia da
+// semana correspondente utilizando a estrutura condicional switch.
+
+/*
+
+const numero = Number(prompt('Informe um numero de 1 a 7: '));
+
+switch (numero) {
+    case 1:
+        alert(`O número ${numero} equivale ao Domingo `);
+        break;
+    case 2:
+        alert(`O número ${numero} equivale a Segunda-feira `);
+        break;
+    case 3:
+        alert(`O número ${numero} equivale a Terça-feira `);
+        break;
+    case 4:
+        alert(`O número ${numero} equivale a Quarta-feira `);
+        break;
+    case 5:
+        alert(`O número ${numero} equivale a Quinta-feira `);
+        break;
+    case 6:
+        alert(`O número ${numero} equivale a Sexta-feira `);
+        break;
+    case 7:
+        alert(`O número ${numero} equivale ao Sábado-feira `);
+        break;
+    default:
+        alert(`Você informou um número errado! `)
+        break;
+}
+
+*/
+
+// Exercicio 3 - Crie um programa que peça um número e verifique se ele é positivo,
+// negativo ou zero.
+
+/*
+const informeNumero = Number(prompt('Informe um numero:'))
+
+if (informeNumero > 0) {
+    alert(`O número ${informeNumero} é positivo!`)
+} else if (informeNumero < 0) {
+    alert(`O número ${informeNumero} é negativo`)
+} else if (informeNumero == 0) {
+    alert(`O número ${informeNumero} é zero!`)
+} else {
+    alert(`Informou algo errado ae filhão!`)
+}
+*/
+
+// Exercicio 4 - escrever um algoritmo para ler o custo de fábrica de um carro, calcular e escrever
+// o custo final ao consumidor.
+
+/*
+
+const custoFabrica = 10000;
+
+const porcenDistri = custoFabrica * 0.28
+
+const impostoFabrica = custoFabrica * 0.45
+
+
+const valorCarro = custoFabrica + porcenDistri + impostoFabrica;
+
+alert(`Valor do carro para o consumidor será de R$ ${valorCarro}`);
+
+*/
+
